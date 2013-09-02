03ebefece2f5d11e6ebbabce526693059991bb24
Eritrea <Eritrea@users.noreply.github.com>
2013-09-02 11:30:18 +0200

    Update yourHeader.php

--- yourHeader.php
+++ yourHeader.php
@@ -1,40 +1,38 @@
 <?php
-require_once('seoWrapperClass.php');
-$_seoWrapper = new SeoWrapper();
 
-/*
-*   Check if Current page is static / dynamic (dynamic == fetch data from db, and display title, cont, desc... from this data)
-*   if not, then treat it as dynamic page
-*/
+	require_once('seoWrapperClass.php');
+	$_seoWrapper = new SeoWrapper();
 
-$StaticPage = $_seoWrapper->Static_Pages();
+	$StaticPage = $_seoWrapper->checkIfPageIsAllowed();
 
+	if($StaticPage !== false){
+		$fetch = $_seoWrapper->getPageProperties();
+		$seo['title'] = $fetch[0];
+		$seo['keywords'] = $fetch[1]['keywords'];
+		$seo['content'] = $fetch[2]['content'];
 
-if ($StaticPage !== false) {
-    $G = $_seoWrapper->Get_Static_Contents();
-    $seo['title'] = $G[0]; // title for current page 
-    $seo['keywords'] = $G[1]['keywords']; //keywords
-    $seo['content'] = $G[2]['content']; //content
+	}else{
 
+		$conn = new PDO('mysql:host=localhost; dbname=Test_SimpleSeo', 'root', '');
+		$fetch = $_seoWrapper->getDynamicContents($conn, 'pages', "id");
 
-} else {
-    $conn = new PDO('mysql:host=localhost; dbname=Test_SimpleSeo', 'root', '');
-    $_seoWrapper->checkErrors();
-    $fetch = $_seoWrapper->Get_Dynamic_Contents($conn, 'pages', "id");
-
-    $seo['title'] = $fetch['title'];
-    $seo['content'] = $fetch['content'];
-    $seo['keywords'] = $fetch['keyword'];
-}
+        $_seoWrapper->checkErrors();
+		$seo['title'] = $fetch['title'];
+		$seo['content'] = $fetch['content'];
+		$seo['keywords'] = $fetch['keyword'];
+	}
 ?>
-
+	
 <!DOCTYPE html>
-<html lang="en-US">
-<head>
-    <meta charset="utf-8"/>
-    <meta name="robots" content="index, follow"/>
-    <meta name="description" content="<?php echo $seo['content']; ?> "/>
-    <meta name="keywords" content="<?php echo $seo['keywords']; ?>"/>
-    <meta name="REVISIT-AFTER" content="15 DAYS"/>
-    <title><?php echo $seo['title'] ?>  </title>
+<html lang="en-US"> 
+	<head> 
+		<meta charset="utf-8" /> 
+		<meta name="robots" content="index, follow" />
+		<meta name="description" content="<?php echo $seo['content']; ?> " />
+		<meta name="keywords" content="<?php echo $seo['keywords']; ?>" />
+		<meta name="REVISIT-AFTER" content="15 DAYS" />
+	<title><?php echo $seo['title'] ?>  </title>
 </head> 
+
+
+
