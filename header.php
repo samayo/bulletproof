2019900695f2f474f145ed0a253f85887441c0ed
Eritrea <Eritrea@users.noreply.github.com>
2013-09-04 21:02:12 +0200

    Update header.php

--- header.php
+++ header.php
@@ -3,20 +3,31 @@
 	require_once('seoWrapperClass.php');
 	$_seoWrapper = new SeoWrapper();
 
-	$StaticPage = $_seoWrapper->checkIfPageIsAllowed();
+
+	$StaticPage = $_seoWrapper->returnPageContent($_SERVER['REQUEST_URI'], parse_url($_SERVER['SCRIPT_NAME']));
+    var_dump($StaticPage);
 
 	if($StaticPage !== false){
-		$fetch = $_seoWrapper->getPageProperties();
-		$seo['title'] = $fetch[0];
-		$seo['keywords'] = $fetch[1]['keywords'];
-		$seo['content'] = $fetch[2]['content'];
+		$title = $_seoWrapper->fetchAllFromStaticPages()[1][$StaticPage];
+		$keyword = $_seoWrapper->fetchAllFromStaticPages()[2];
+		$content = $_seoWrapper->fetchAllFromStaticPages()[3];
+
+	var_dump($title);
+	var_dump($keyword);
+	var_dump($content);
+
+
 
 	}else{
 
 		$conn = new PDO('mysql:host=localhost; dbname=Test_SimpleSeo', 'root', '');
 		$fetch = $_seoWrapper->getDynamicContents($conn, 'pages', "id");
 
-                $checkError =  $_seoWrapper->checkErrors();
+        $errorChecking = $_seoWrapper->checkErrors();
+        var_dump($errorChecking);
+        ($errorChecking) ? die('Page not found') : '';
+        // ^^ if page is not found or query failed, do your monkey business here.
+
 		$seo['title'] = $fetch['title'];
 		$seo['content'] = $fetch['content'];
 		$seo['keywords'] = $fetch['keyword'];
