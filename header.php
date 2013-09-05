0ce77ed8355d0d6e737caec83824f475fbe868d9
Eritrea <Eritrea@users.noreply.github.com>
2013-09-05 08:50:42 +0200

    Update header.php

--- header.php
+++ header.php
@@ -4,44 +4,44 @@
 	$_seoWrapper = new SeoWrapper();
 
 
-	$StaticPage = $_seoWrapper->returnPageContent($_SERVER['REQUEST_URI'], parse_url($_SERVER['SCRIPT_NAME']));
-    var_dump($StaticPage);
+	$StaticPage = $_seoWrapper->returnPageContent()->forUrl($_SERVER['REQUEST_URI'], parse_url($_SERVER['SCRIPT_NAME']));
+
 
 	if($StaticPage !== false){
-		$title = $_seoWrapper->fetchAllFromStaticPages()[1][$StaticPage];
-		$keyword = $_seoWrapper->fetchAllFromStaticPages()[2];
-		$content = $_seoWrapper->fetchAllFromStaticPages()[3];
 
-	var_dump($title);
-	var_dump($keyword);
-	var_dump($content);
-
-
+        $title = $_seoWrapper->fetchAllFromStaticPages()[1][$StaticPage];
+        $content = $_seoWrapper->fetchAllFromStaticPages()[2][0];
+        $keywords = $_seoWrapper->fetchAllFromStaticPages()[3][0];
 
 	}else{
 
-		$conn = new PDO('mysql:host=localhost; dbname=Test_SimpleSeo', 'root', '');
+		$conn = new PDO('mysql:host=localhost; dbname=seoWrapper', 'root', '');
 		$fetch = $_seoWrapper->getDynamicContents($conn, 'pages', "id");
 
-        $errorChecking = $_seoWrapper->checkErrors();
-        var_dump($errorChecking);
-        ($errorChecking) ? die('Page not found') : '';
+
+        ($_seoWrapper->checkErrors()) ? die('Page not found') : '';
         // ^^ if page is not found or query failed, do your monkey business here.
 
-		$seo['title'] = $fetch['title'];
-		$seo['content'] = $fetch['content'];
-		$seo['keywords'] = $fetch['keyword'];
+        $title = $fetch['title'];
+        $content = $fetch['content'];
+        $keywords = $fetch['keywords'];
+
+
+
+
+
 	}
 ?>
+	
 <!DOCTYPE html>
 <html lang="en-US"> 
 	<head> 
 		<meta charset="utf-8" /> 
 		<meta name="robots" content="index, follow" />
-		<meta name="description" content="<?php echo $seo['content']; ?> " />
-		<meta name="keywords" content="<?php echo $seo['keywords']; ?>" />
+		<meta name="description" content="<?php echo $content; ?> " />
+		<meta name="keywords" content="<?php echo $keywords; ?>" />
 		<meta name="REVISIT-AFTER" content="15 DAYS" />
-	<title><?php echo $seo['title'] ?>  </title>
+	<title><?php echo $title; ?>  </title>
 </head> 
 
 
