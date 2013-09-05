7ef37a2c7063f46fb8efadea52e5a3dcd19666f2
Eritrea <Eritrea@users.noreply.github.com>
2013-09-05 08:50:56 +0200

    Update seoWrapper.php

--- seoWrapper.php
+++ seoWrapper.php
@@ -2,34 +2,33 @@
 
 class SeoWrapper{
     private $_errors = [];
-    private  $siteName = ' | YourSiteNamehere.tld '; // leave '' if not needed
-
+    public $foo;
 
     function fetchAllFromStaticPages(){
 
         $existingStaticPages = [
-            '/seowrapper/headder.php',
             '/seowrapper/header.php',
+            '/seowrapper/headder.php',
             '/seowrapper/password.php?task=change',
             '/seowrapper/password.php?task=forgot'
         ];
 
 
         $staticPageTitles = [
-            'title'=>'Welcome to my site, this is index page',
-            'title'=>'this is the about page',
-            'title'=>'So, you want to change your password ehh?',
-            'title'=>'Ok! your password has been sent'
+            'Welcome to my site, this is index page',
+            'this is the about page',
+            'So, you want to change your password ehh?',
+            'Ok! your password has been sent'
         ];
 
 
         $staticPageKeywords = [
-            'keywords'=>'this, is, where, your, site, keywords, go, separated, by, commas,'
+           'this, is, where, your, site, keywords, go, separated, by, commas,'
         ];
 
 
         $staticPageDescription = [
-            'content'=>'This is where the (content) of your meta site goes'
+           'This is where the (content) of your meta site goes'
         ];
 
          return  [$existingStaticPages, $staticPageTitles, $staticPageKeywords, $staticPageDescription];
@@ -37,23 +36,24 @@ class SeoWrapper{
 
 
 
-    function returnPageContent($requestUri, $scriptName){
+    function returnPageContent(){
 
-        $existingStaticPages = $this->fetchAllFromStaticPages()[0];
+         function forUrl($requestUri, $scriptName){
+            $existingStaticPages = $this->fetchAllFromStaticPages()[0];
 
-        if(in_array($requestUri, $existingStaticPages)){
-            $pageKey =  array_search($requestUri, $existingStaticPages);
-           return $pageKey;
-           // return $this->declaredPageProperties()[0][$pageKey];
+            if(in_array($requestUri, $existingStaticPages)){
+                $pageKey =  array_search($requestUri, $existingStaticPages);
+               return $pageKey;
 
-        }else if(in_array($scriptName, $existingStaticPages)){
-            $pageKey =  array_search($scriptName, $existingStaticPages);
-                return $pageKey;
-          //  return $this->declaredPageProperties()[0][$pageKey];
+            }else if(in_array($scriptName, $existingStaticPages)){
+                $pageKey =  array_search($scriptName, $existingStaticPages);
+                    return $pageKey;
 
-        }else{
-            return $this->_errors = 'Current url is not found in allowed static pages';
+            }else{
+                return $this->_errors = false;
+            }
         }
+
     }
 
 
@@ -67,11 +67,12 @@ class SeoWrapper{
             return $this->_errors = 'Invalid URL is found';
         }else{
             try{
-                $stmt = $conn->prepare("SELECT * FROM '$table' WHERE id = ? ");
+                $stmt = $conn->prepare("SELECT title,content,keywords FROM $table WHERE id = ? ");
                 $stmt->execute([$_GET[$identifier]]);
             }catch(PDOException $e){
                 return $this->_errors = 'Unknown error occured. Please try again'.$e->getMessage();
             }
+
 
             if($stmt->rowCount() == 0){
                 return $this->_errors  = 'Page not found! Link may be invalid or expired';
