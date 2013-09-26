5419abd11865a85660aaa4a9afe3aa454284f353
simon|er <simon@habeshawebsite.com>
2013-09-27 01:13:53 +0200

    re-wrote the whole thing, fixed bugs

--- src/StaticPages.php
+++ src/StaticPages.php
@@ -3,58 +3,50 @@
 /**
  * Class staticPages
  *
- * this is where all our static pages and a title for each page is stored. The description is option (see second page, it has none)
- * if you don't specify description for the pages, then the one from 'defaultPageSettings' or next trait will be used.
+ * this is where all our static pages and a title for each page is stop
  *
  */
-trait staticPages{
-    public function myStaticPages(){
-        return [
-            'Pages'=>[
-                    '/seowrapper/demo.php'=> [ 
-							'title'=>'I am title for demo page', 
-							'description'=>'I am optional description for demo page' ],
-							
-                    '/seowrapper/contact.php'=> [
-							'title'=>'I am title for contact page',
-                    ]
-             ]
-		];
-    }
 
+function definePages(){
+    return [
+        'staticPages'=>[
+
+            '/seowrapper/demo.php'=> [
+                 'title'=>'I am title for demo page',
+                 'description'=>'this is demo pages description'],
+
+            '/seowrapper/contact.php'=> [
+                 'title'=>'I am title for contact page',
+                 'decription'=>'I am optional description for test page'],
+        ],
+
+
+        'dynamicPages'=>[
+                  'title'=>'I am title for dynamic page without any query',
+                  'description'=>'I am obligatory description for test page'],
+    ];
 }
 
 
-/**
- * Class defaultPageSettings
- *
- * By default, all your static pages will use the keyword array, the description however is optional. If user
- * omits/forgets to mention the description tag, then this one will be used.
- */
 
-trait defaultPageSettings{
-    public function myDefaultSettings(){
-        return [
-            'description'=>[
-                'this is the default page description for all your static pages'],
-            'keywords'=>[
-                'this, should, be, keywords, for, all, your, pages, seperated, by commas']
-        ];
-    }
+
+
+function myDefaultSettings(){
+    return [
+        'keywords'=>[
+            'this, should, be, keywords, for, all, your, pages, seperated, by commas']
+    ];
 }
 
 
-/**
- * Class databaseConfigs
- *
- * $TableName - Specify with table gets fetched for content, when your dynamic pages are fetching for a result.
- * $queryType - is 'id' or type of the query string
- * $dataToFetch - tells the what rows to fetch from $tableName
- */
-trait databaseConfigs{
-	public $tableName = 'pages';
-	public $queryType = 'id';
-	public $dataToFetch = ['title','keywords','description'];
+
+function databaseConfigs(){
+     return [
+        'tableName'=>'pages',
+        'queryType'=>'id',
+        'dataToFetch'=>['title','keywords','description']
+    ];
+
 }
 
 
