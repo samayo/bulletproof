b5b5d0fde5636559b262a19154b57e1808417702
simon|er <simon@habeshawebsite.com>
2013-09-27 01:52:39 +0200

    now, its easier to understand

--- src/configMe.php
+++ src/configMe.php
@@ -1,21 +1,27 @@
 <?php
 
 /**
- * Class staticPages
+ *  This is the file, in which your entire configuration should take place
+ */
+
+
+/**
+ * Name your static pages,
+ *  - title and description are obligatory for each new page you create.
+ *  - dynamic pages only need title, and description you don't have to name your dynamic pages
  *
- * this is where all our static pages and a title for each page is stop
- *
+ * @return array
  */
 
 function definePages(){
     return [
         'staticPages'=>[
 
-            '/seowrapper/demo.php'=> [
+            '/demo.php'=> [
                  'title'=>'I am title for demo page',
                  'description'=>'this is demo pages description'],
 
-            '/seowrapper/contact.php'=> [
+            '/contact.php'=> [
                  'title'=>'I am title for contact page',
                  'decription'=>'I am optional description for test page'],
         ],
@@ -28,9 +34,11 @@ function definePages(){
 }
 
 
-
-
-
+/**
+ * this is the keyword for all your static pages, configure it well.
+ *
+ * @return array
+ */
 function myDefaultSettings(){
     return [
         'keywords'=>[
@@ -39,6 +47,16 @@ function myDefaultSettings(){
 }
 
 
+/**
+ * this setting deals specifically with dynamic part of your page. It will help you query database for some rows
+ * tableName   = provide here, which table you want to pull data from, for your dynamic content
+ * queryType   = specify here, what your get parameter, is it $_GET['id'] or $_GET['article'] or $_GET['whatever']
+ *               by default, I am assuming your dynamic page uses $_GET['id']. So, the "id" is already mentioned
+ * dataToFetch = for the purpose of this example, we are fetching only three rows from database and have integrated them inside
+ *               the header, but you can fetch 20 rows if you want and stuff them in the metadata, ... it's your choice now :)
+ *
+ * @return array
+ */
 
 function databaseConfigs(){
      return [
