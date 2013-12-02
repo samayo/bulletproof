56514a481f3db18cdd64a88cfdc994571a76732f
QQ <Eritrea@users.noreply.github.com>
2013-12-02 18:19:27 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,34 +1,38 @@
 <?php
 /**
- * A small, secure & fast image uploader class written in all-static
- * class to give an extra boost in performance.
- * @author     Simon _eQ <https://github.com/simon-eQ>
- * @license    Public domain. Do anything you want with it.
- *
- */
-
-    include_once 'BulletProof.php';
-
-    BulletProof::options(array('png', 'jpeg', 'gif', 'jpg'),
-						 array('max-width'=>150, 'max-height'=>150),
-						 30000,
-						'pictures/'
-						);
+* A small, secure & fast image uploader class written in all-static
+* class to give an extra boost in performance.
+* @author     Simon _eQ <https://github.com/simon-eQ>
+* @license    Public domain. Do anything you want with it.
+*
+*/
 
 
+include 'BulletProof2.php';
 
-    $renamePicture = "Passport_Picture";
+$BulletProof = new BulletProof(array('jpg', 'png', 'gif', 'jpeg'));
 
-    if($_FILES){
-        $upload =  BulletProof::upload($_FILES['profile_pic'], $renamePicture);
-      
-        if($upload === true){
-            echo "Success! Your image is uploaded";
-        }
-	}
-	
-	
-	   echo "<form method='POST' enctype='multipart/form-data'>
-			 <input type='hidden' name='MAX_FILE_SIZE' value='30000' />
-			 <input type='file' name='profile_pic' />
-	  		 <input type='submit' value='upload' />";
+if($_FILES):
+
+
+$result = $BulletProof->setImageDimensions(array('max-height'=>150, 'max-width'=>150))
+                      ->setFileSize(array('max-size'=>4000, 'min-size'=>1))
+                      ->setUploadDir('uploads/')
+                      ->upload($_FILES['logo']);
+
+
+
+echo $result; //242i42923.jpg
+
+endif;
+
+
+
+
+?>
+
+<form method="POST" enctype="multipart/form-data">
+<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+<input type="file" name="logo" id="file"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
