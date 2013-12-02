abe5c0e8e6315c0d159d16e7d710811237bfa5d3
QQ <Eritrea@users.noreply.github.com>
2013-12-02 18:24:53 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,38 +1,40 @@
+EXAMPLE 1
 <?php
 /**
-* A small, secure & fast image uploader class written in all-static
-* class to give an extra boost in performance.
-* @author     Simon _eQ <https://github.com/simon-eQ>
-* @license    Public domain. Do anything you want with it.
-*
-*/
+ * A small, secure & fast image uploader class written in all-static
+ * class to give an extra boost in performance.
+ * @author     Simon _eQ <https://github.com/simon-eQ>
+ * @license    Public domain. Do anything you want with it.
+ *
+ */
 
 
-include 'BulletProof2.php';
+include 'BulletProof.php';
 
-$BulletProof = new BulletProof(array('jpg', 'png', 'gif', 'jpeg'));
+$Obj = new BulletProof(array('jpg', 'png', 'gif', 'jpeg'));
 
 if($_FILES):
-
-
-$result = $BulletProof->setImageDimensions(array('max-height'=>150, 'max-width'=>150))
-                      ->setFileSize(array('max-size'=>4000, 'min-size'=>1))
-                      ->setUploadDir('uploads/')
-                      ->upload($_FILES['logo']);
-
-
-
-echo $result; //242i42923.jpg
-
+    $result = $Obj->setImageDimensions(array('max-height'=>150, 'max-width'=>150))
+        ->setFileSize(array('max-size'=>4000, 'min-size'=>1))
+        ->setUploadDir('uploads/')
+        ->upload($_FILES['logo']);
+    echo $result; //242i42923.jpg
 endif;
-
-
-
-
 ?>
+<hr/>
 
-<form method="POST" enctype="multipart/form-data">
-<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-<input type="file" name="logo" id="file"><br>
-<input type="submit" name="submit" value="Submit">
-</form>
+EXAMPLE 2
+<hr/>
+
+<?php
+include 'BulletProof.php';
+
+$Obj = new BulletProof(array('jpg', 'png', 'gif', 'jpeg'),
+                       array('max-height'=>150, 'max-width'=>150),
+                       array('max-size'=>4000, 'min-size'=>1),
+                       'uploads/');
+
+if($_FILES){
+        ->upload($_FILES['logo'], 'passport_pic');
+        echo $result; //passport_pic.jpg
+}
