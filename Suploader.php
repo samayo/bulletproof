52c903669976283572568d822884d124acbb62c9
QQ <Eritrea@users.noreply.github.com>
2013-12-04 01:19:09 +0200

    Update Suploader.php

--- Suploader.php
+++ Suploader.php
@@ -1,22 +1,24 @@
 <?php
 
-namespace SecureUploader;
+namespace ImageUploader;
 
 /**
- * A secure and simple image uploader class. You may upload files to,
- * but it is best suited for images such as jpg, gif, png types/variations.
+ * ImageUploder: A simple and secure PHP Image uploader class.
+ *
+ * You may upload any files too, but for best performance, use this class
+ * to upload images only. As, it is best made for checking & validating
+ * images, such as the jpg, gif, png types/variations.
  * @author     Simon _eQ <https://github.com/simon-eQ>
- * @license    Public domain. Do anything you want with it.
+ * @license    Public domain. No Licence.
  */
 
 
-class Suploader
+class ImageUploader
 {
 
     /**
-     * Create a set of default properties, just in case if you are uploading
-     * the same size, dimension and type of images to same folder. If not,
-     * these values can be overridden later.
+     * Create a set of default properties for image size, types, dimensions,
+     * these settings can be overridden later through getters/setters
      * @var array
      */
     private $fileType            = array("jpg", "png", "gif");
@@ -26,7 +28,7 @@ class Suploader
 
 
     /**
-     * Set file size (in bytes) by giving an array value with max and min sizes
+     * Set file size (in bytes) by passing an array value with max and min sizes
      * @param array $setFileSize ex: array("min"=>100, "max"=>30000);
      * @return $this
      */
@@ -37,7 +39,7 @@ class Suploader
     }
 
     /**
-     * Set file MIME type, as many as you want
+     * Set file MIME type you want users to upload
      * @param array $setFileType ex: array("jpg", "png");
      * @return $this
      */
@@ -48,9 +50,9 @@ class Suploader
     }
 
     /**
-     * If this method is called and set, then this class will asume
-     * you are trying to upload an image, so during file upload
-     * image size validation will take place.
+     * If this method set, then this class will assume every upload is
+     * an image type, and will try to check dimensions from it to validate
+     * it as an image. For uploading a non-image file, don't call this class.
      * @param array $setDimensions
      * @return $this
      */
@@ -61,7 +63,7 @@ class Suploader
     }
 
     /**
-     * Set a directory to upload the files into.
+     * Set a folder/directory to upload the files into.
      * @param $folderName ex: 'uploads/'
      * @return $this
      */
@@ -73,14 +75,15 @@ class Suploader
 
 
     /**
-     * Check for common file upload errors thrown by the $_FILE[]['error'] global
+     * Create a costume array for each possible error that may be
+     * thrown by the $_FILES[] array.
      * @return array
      */
     public function commonFileUploadErrors()
     {
         /**
-         * We can use the key identifier from $_FILES[]['error'] to output
-         * the corresponding errors messages. Damn I'm good! :D
+         * We can use those keys as identifiers of the $_FILES[]['error'] value
+         * to call the corresponding error messages. Damn I'm good! :D
          */
         return array(
             UPLOAD_ERR_OK           => "...",
@@ -94,37 +97,35 @@ class Suploader
     }
 
 
-
-
     /**
      * There are many reasons for a file upload not work, other than from the information
      * obtained by the $_FILES[]['error'] array, So, this function tends to debug server
-     * environment for a possible cause of an error as a last resort, if file uploaded wasn't a success
-     * @param null $newDirectory optional directory, if not specified this class will use tmp_name
+     * environment for a possible cause of an error, if file uploaded was not a success.
+     * @param null $newDirectory optional directory, if not specified this method  will use tmp_name
      * @return string
      */
     public function debugEnviroment($newDirectory = null)
     {
         /**
-         * If user has specified upload dir, check and debug it first otherwise,
-         * check the default dir given by PHP
+         * If given a new directory to upload the files, then check and debug it first
+         * otherwise, check the temporary default dir given by PHP i.e. 'tmp_name'
          */
         $uploadFileTo = $newDirectory ? $newDirectory : init_get("file_uploads");
 
+
         /**
-         * check the directory (if) specified by user is indeed dir or not
+         * Check if the given upload folder, is a valid directory
          */
-        if(!is_dir($uploadFileTo))
-        {
+        if(!is_dir($uploadFileTo)){
             return "Please make sure this is a valid directory, or php 'file_uploads' is turned on";
         }
+
 
         /**
          * Still not sure how this is done. But, I am trying to check check if given
          * upload directory has write permissions
          */
-        if(!substr(sprintf('%o', fileperms($uploadFileTo)), -4) != 0777)
-        {
+        if(!substr(sprintf('%o', fileperms($uploadFileTo)), -4) != 0777){
             return "Sorry, you don't have her majesty's permission to upload files on this server";
         }
 
@@ -132,9 +133,9 @@ class Suploader
 
 
     /**
-     * A final method to validate, rename and upload the file.
-     * @param $fileToUpload
-     * @param $newFileName
+     * The final method that validates, renames and uploads the image/file.
+     * @param $fileToUpload ex: $_FILES['name'];
+     * @param $newFileName  ex: 'uploads/'
      * @return bool|string
      */
     public function upload($fileToUpload, $newFileName = null)
@@ -142,97 +143,104 @@ class Suploader
 
         /**
          * First let's start with the easiest method, by checking if
-         * $_FILES['name']['error'] is not '0'. (means there is an error)
+         * $_FILES['name']['error'] is not '0'.  If so, then there is
+         * an error thrown by the $_FILES array.
          */
-        if($fileToUpload['error'])
-        {
+        if($fileToUpload['error']){
             $errors = $this->commonFileUploadErrors();
             return $errors[$fileToUpload['error']];
         }
 
 
         /**
-         * Since the file type provided by $_FILES is unreliable due to
-         * system/browser variations, we will double check the real type/extension
-         * with SplFileInfo::getExtension();
+         * Since the file type provided by $_FILES is unreliable due to system/browser
+         * variations, we will double check the real type/extension
+         * with the SplFileInfo::getExtension(); method.
          */
-
         $splFileInfo       = new SplFileInfo($fileToUpload['name']);
         $splFileExtension  = $splFileInfo->getExtension();
 
+
         /**
-         * get rid of the 'image/' part from ex: 'image/gif' to get the extension
+         * Get rid of the 'image/' part from ex: 'image/gif' to get the extension
          * so we can compare it with what is expected. ex: array('jpg','png',''gif)
          */
         $fileTypeExtension = substr($fileToUpload['type'], 6);
 
+
         /**
          * Since 'SplFileInfo::getExtension()' and FILES[]['type'] give often different
          * names even for the same file, ex: "jpeg vs jpg" ".doc vs application/msword"
-         * We can't really check if both are identical, so the best bet is to check
-         * if they are both inside the $allowedMimeTypes set by the user
+         * We can't really check if both results are identical, so the best bet is to check
+         * if they are both inside the $allowedMimeTypes.
          */
         if(!in_array($fileTypeExtension, $this->fileExtensions) ||
-           !in_array($splFileExtension, $this->fileExtensions))
-        {
+           !in_array($splFileExtension, $this->fileExtensions)){
             return "This is not allowed File type. Please only upload ("
                 . implode(' ,', $this->fileExtensions) .") file types";
         }
 
         /**
-         * Once file is validated and is OK, retain the real extension for a later use
+         * retain the real extension type, once the image/file is validated.
          */
         $realFileExtension = ".".$splFileExtension;
 
 
         /**
-         * Check if file size is within the scope of what the user has defined.
+         * Check if the Min & Max file size is within the scope of what
+         * is expected in the setFileSize() method.
          */
         if($fileToUpload['size'] < $this->fileSize['min'] ||
-            $fileToUpload['size'] > $this->fileSize['max'])
-        {
+            $fileToUpload['size'] > $this->fileSize['max']){
             return "Min & Max file sizes must be less in-between
                     ".(implode(" to ", $this->fileSize))." kilobytes";
         }
 
 
         /**
-         * IMPORTANT:
-         * If the value for "$this->allowedFileDimensions", is set, it means the uploaded image must be
-         * validated as an image. Meaning, we can check the dimensions of the image in pixels.
-         * If not set however, checking for image dimensions is not relevant here
+         * Important!
+         * If a value is passed through setImageDimensions(); then, this class
+         * will validate the uploaded material as if it was an image, by checking the height
+         * and width for pixels. If value is set to NULL however, this class will
+         * not check height/width. So, you can upload a normal file
          */
-        if($this->imageDimensions)
-        {
+        if($this->imageDimensions){
+
+            /**
+             * Get width and height of the image file for validation
+             */
             list($width, $height, $type, $attr) = getimagesize($fileToUpload['tmp_name']);
 
             if($width > $this->imageDimensions['max-width'] ||
-               $height > $this->imageDimensions['min-width'])
-            {
+               $height > $this->imageDimensions['min-width']){
                 return "Image must be less than "
                         .$this->imageDimensions['max-width']."pixels wide and"
                         .$this->imageDimensions['max-height']."pixels in height";
             }
 
-
-            if($height <= 1 || $width <=1)
-            {
+            /**
+             * If height/width are smalled than 1, the image is unlikely to be valid.
+             */
+            if($height <= 1 || $width <=1){
                 return "This file is either too small or corrupted to be an image file";
             }
 
         }
 
+
+
         /**
-         * Check whether user has passed a second argument as a new name for this file
+         * check weather, a new name is assigned for this file, through the second argument.
          */
-        if($newFileName)
-        {
+        if($newFileName){
+
             /**
-             * If given a file name, then assign it and append the new extension obtained
+             * If a file name is set, then assign it and append the new extension obtained
              * from the SplFileInfo::getExtension();
              */
             $newFileName = $newFileName.$realFileExtension;
         }else{
+
 
             /**
              * If not, create a 54 digit length id for the file.
@@ -249,24 +257,20 @@ class Suploader
          */
         $checkSafeUpload = is_uploaded_file($fileToUpload['tmp_name']);
 
+
         /**
          * Move the file to the new dir specified by user
          */
-        $moveUploadFile = move_uploaded_file($fileToUpload['tmp_name'],
-                                            $this->uploadFolder.'/'.
-                                            $newFileName);
+        $moveUploadFile = move_uploaded_file($fileToUpload['tmp_name'], $this->uploadFolder.'/'.$newFileName);
 
 
         /**
          * Check if every validation has gone as expected.
-         * If true, return the new file name with the extension as a positive response.
+         * If true, return the new file name with its extension as a positive response.
          */
-        if($checkSafeUpload && $moveUploadFile)
-        {
+        if($checkSafeUpload && $moveUploadFile){
             return $newFileName;
-        }
-        else
-        {
+        }else{
             /**
              * If file upload has not worked for any reason, the debug the server environment and its
              * permissions, settings [more to be added] etc.. for possible errors.
