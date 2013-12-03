6699656147b42d533ed8db40e88200de71658abe
QQ <Eritrea@users.noreply.github.com>
2013-12-03 04:20:23 +0200

    Update example.php

--- example.php
+++ example.php
@@ -2,48 +2,29 @@
 /***************************************************************************
  *  EXAMPLE 1                                                              *
  ***************************************************************************
- * First example enables you to upload different file sizes, dimensions into
- * different upload directories. This can be good, if your website allows
- * users to upload with different types of restrictions.
- * Meaning on each page you put this script on, you get to declare different
- * options for your uploader.
+ * The Suploder class has default setting of
+ *
+ * private $fileSize         = array("min"=>100, "max"=>30000);
+ * private $fileExtensions   = array("jpg", "png", "gif");
+ * private $imageDimensions  = array("max-height"=>1150, "max-width"=>1150);
+ * private $uploadFolder     = "uploads/";
+ *
+ * If you want to change those setting during upload, then see below.
  */
 
 
-include 'Suploader.php';
+include 'Suploder.php';
 
-$Obj = new Suploader(array('jpg', 'png', 'gif', 'jpeg'));
+$Obj = new Suploder;
+
 
 if($_FILES){
-    $result = $Obj->setImageDimensions(array('max-height'=>150, 'max-width'=>150))
-        ->setFileSize(array('max-size'=>4000, 'min-size'=>1))
-        ->setUploadDir('uploads/')
-        ->upload($_FILES['logo']);
-    echo $result; //242i42923.jpg
+      $result = $Obj->setFileType(array("jpg", "gif"))
+                    ->setFileSize(array("min"=>1, "max"=>100))
+                    ->setImageDimensions("max-height"=>450, "max-width"=>550)
+                    ->setFolder('uploads/')
+                    ->upload($_FILES['logo']);
+    echo $result; //345212631129223425311217529118879612810120122102746529cc1c8d909c1.40357962.jpg
 }
 
 
-
-
-
-/***************************************************************************
- *  EXAMPLE 2                                                              *
- ***************************************************************************
- * This example allows you to set only one option, meaning whenever you use
- * `$Obj->upload()` all files to be uploaded must follow the global setting
- * you have desalted in your constructor. So, this is only usefull, if you
- * are going to upload all files with only one setting ie size, dimensions
- * directory....
- * 
- */
-
-
-$Obj = new Suploader(array('jpg', 'png', 'gif', 'jpeg'),
-                       array('max-height'=>150, 'max-width'=>150),
-                       array('max-size'=>4000, 'min-size'=>1),
-                       'uploads/');
-
-if($_FILES){
-    $Obj->upload($_FILES['logo'], 'passport_pic');
-        echo $result; //passport_pic.jpg
-}
