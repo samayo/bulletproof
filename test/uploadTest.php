f13216e1c4c3ca20ccccf23a3fb1733040356f7c
samayo <samayo@protonmail.ch>
2017-07-28 16:35:30 -0700

    add backward compatability to phpunit

--- test/uploadTest.php
+++ test/uploadTest.php
@@ -1,7 +1,13 @@
 <?php
-use PHPUnit\Framework\TestCase; 
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
  
-class uploadTest extends  TestCase
+class uploadTest extends  \PHPUnit\Framework\TestCase
 {
     public $bulletproof,
         $testingImage,
