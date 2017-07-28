55737bfc6cbfe9b3d03099e16edc00273af68939
samayo <samayo@protonmail.ch>
2017-07-28 16:38:55 -0700

    remove \ to support php5.3 testing with phpunit

--- test/uploadTest.php
+++ test/uploadTest.php
@@ -3,7 +3,7 @@
 // backward compatibility
 if (!class_exists('\PHPUnit\Framework\TestCase') &&
     class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+    class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
 
  
