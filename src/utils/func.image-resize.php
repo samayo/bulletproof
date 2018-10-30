45f9d1bdfcf9c5cc299e5355affd69649aa1bf55
Juraj Lörinc <lorinc.juraj@gmail.com>
2018-10-30 19:12:08 +0100

    Added image cropping feature to image resizing utility (backwards compatible)

--- src/utils/func.image-resize.php
+++ src/utils/func.image-resize.php
@@ -14,46 +14,99 @@
  */
 namespace Bulletproof\Utils;
 
-function resize($image, $mimeType, $imgWidth, $imgHeight, $newWidth, $newHeight, $ratio = false, $upsize = true)
+function resize($image, $mimeType, $imgWidth, $imgHeight, $newWidth, $newHeight, $ratio = false, $upsize = true, $cropToSize = false)
 {
-
-    // First, calculate the height.
-    $height = intval($newWidth / $imgWidth * $imgHeight);
-
-    // If the height is too large, set it to the maximum height and calculate the width.
-    if ($height > $newHeight) {
-        $height = $newHeight;
-        $newWidth = intval($height / $imgHeight * $imgWidth);
-    }
-
-    // If we don't allow upsizing check if the new width or height are too big.
-    if (!$upsize) {
-        // If the given width is larger than the image width, then resize it
-        if ($newWidth > $imgWidth) {
-            $newWidth = $imgWidth;
-            $height = intval($newWidth / $imgWidth * $imgHeight);
-        }
-
-        // If the given height is larger then the image height, then resize it.
-        if ($newHeight > $imgHeight) {
-            $newHeight = $imgHeight;
-            $newWidth = intval($newHeight / $imgHeight * $imgWidth);
-        }
-    }
-
-    if ($ratio == true) {
+    // Checks whether image cropping is enabled
+    if ($cropToSize) {
         $source_aspect_ratio = $imgWidth / $imgHeight;
         $thumbnail_aspect_ratio = $newWidth / $newHeight;
-        if ($imgWidth <= $newWidth && $imgHeight <= $newHeight) {
-            $newWidth = $imgWidth;
-            $newHeight = $imgHeight;
-        } elseif ($thumbnail_aspect_ratio > $source_aspect_ratio) {
-            $newWidth = (int)($newHeight * $source_aspect_ratio);
-            $newHeight = $newHeight;
+
+        // Adjust cropping area and position depending on original and cropped image
+        if ($thumbnail_aspect_ratio < $source_aspect_ratio) {
+            $src_h = $imgHeight;
+            $src_w = $imgHeight * $thumbnail_aspect_ratio;
+            $src_x = (int) (($imgWidth - $src_w)/2);
+            $src_y = 0;
         } else {
-            $newWidth = $newWidth;
-            $newHeight = (int)($newWidth / $source_aspect_ratio);
+            $src_w = $imgWidth;
+            $src_h = (int) ($imgWidth / $thumbnail_aspect_ratio);
+            $src_x = 0;
+            $src_y = (int) (($imgHeight - $src_h)/2);
         }
+
+        // Checks whether image upsizing is enabled
+        if (!$upsize) {
+            $newHeightOrig = $newHeight;
+            $newWidthOrig = $newWidth;
+
+            // If the given width is larger than the image width, then resize it
+            if ($newWidth > $imgWidth) {
+                $newWidth = $imgWidth;
+                $newHeight = (int) ($newWidth / $imgWidth * $imgHeight);
+                if ($newHeight > $newHeightOrig) {
+                    $newHeight = $newHeightOrig;
+                    $src_h = $newHeight;
+                    $src_y = (int) (($imgHeight - $src_h)/2);
+                }
+
+                $src_x=0;
+                $src_w = $imgWidth;
+            }
+
+            // If the given height is larger then the image height, then resize it.
+            if ($newHeightOrig > $imgHeight) {
+                $newHeight = $imgHeight;
+                $src_y=0;
+                $src_h = $imgHeight;
+                $src_w = (int) ($src_h * ( $newWidth / $newHeight ));
+                $src_x = (int) (($imgWidth - $src_w)/2);
+            }
+        }
+    } else {
+
+        // First, calculate the height.
+        $height = (int) ($newWidth / $imgWidth * $imgHeight);  //  75
+
+        // If the height is too large, set it to the maximum height and calculate the width.
+        if ($height > $newHeight) {
+            $height = $newHeight;
+            $newWidth = (int) ($height / $imgHeight * $imgWidth);
+        }
+
+        // If we don't allow upsizing check if the new width or height are too big.
+        if (!$upsize) {
+            // If the given width is larger than the image width, then resize it
+            if ($newWidth > $imgWidth) {
+                $newWidth = $imgWidth;
+                $newHeight = (int) ($newWidth / $imgWidth * $imgHeight);
+            }
+
+            // If the given height is larger then the image height, then resize it.
+            if ($newHeight > $imgHeight) {
+                $newHeight = $imgHeight;
+                $newWidth = (int) ($height / $imgHeight * $imgWidth);
+            }
+        }
+
+        if ($ratio == true) {
+            $source_aspect_ratio = $imgWidth / $imgHeight;
+            $thumbnail_aspect_ratio = $newWidth / $newHeight;
+            if ($imgWidth <= $newWidth && $imgHeight <= $newHeight) {
+                $newWidth = $imgWidth;
+                $newHeight = $imgHeight;
+            } elseif ($thumbnail_aspect_ratio > $source_aspect_ratio) {
+                $newWidth = (int) ($newHeight * $source_aspect_ratio);
+                $newHeight = $newHeight;
+            } else {
+                $newWidth = $newWidth;
+                $newHeight = (int) ($newWidth / $source_aspect_ratio);
+            }
+        }
+
+        $src_x = 0;
+        $src_y = 0;
+        $src_w = $imgWidth;
+        $src_h = $imgHeight;
     }
 
     $imgString = file_get_contents($image);
@@ -67,12 +120,12 @@ function resize($image, $mimeType, $imgWidth, $imgHeight, $newWidth, $newHeight,
         $imageFromString,
         0,
         0,
-        0,
-        0,
+        $src_x,
+        $src_y,
         $newWidth,
         $newHeight,
-        $imgWidth,
-        $imgHeight
+        $src_w,
+        $src_h
     );
 
     switch ($mimeType) {
